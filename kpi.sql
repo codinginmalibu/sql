2cd76adc99467af17e3cf068a7cae45e4d74cab4
surfingonrails <smistry@spark.net>
2016-06-05 12:55:05 -0700

    basics, daily revenue, and dau

---
+++ kpi.sql
@@ -0,0 +1,46 @@
+-- KPIs, or key performance indicators for a company
+
+/* =================
+Database Schema
+
+purchases
+
+id	int
+user_id	int
+price	real
+refunded_at	text
+created_at	text
+
+gameplays
+
+id	int
+user_id	int
+created_at	text
+platform	text
+
+================= */
+
+-- purchases
+select *
+from purchases
+order by id
+limit 10;
+
+-- gameplays
+select *
+from gameplays
+order by id
+limit 10;
+
+-- Daily Revenue
+select date(created_at), round( sum(price), 2 ) as daily_rev
+from purchases
+where refunded_at not null
+group by 1
+order by 1;
+
+-- DAU - Daily Active Users
+select date(created_at), count(distinct user_id) as dau
+from gameplays
+group by 1
+order by 1;
