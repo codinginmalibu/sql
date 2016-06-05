259880a512ef74608c48be7fc0c6460f64e060dc
surfingonrails <smistry@spark.net>
2016-06-05 13:29:18 -0700

    arppu and arpu

--- kpi.sql
+++ kpi.sql
@@ -35,12 +35,19 @@ limit 10;
 -- Daily Revenue
 select date(created_at), round( sum(price), 2 ) as daily_rev
 from purchases
-where refunded_at not null
+where refunded_at is null
 group by 1
 order by 1;
 
 -- DAU - Daily Active Users
-select date(created_at), count(distinct user_id) as dau
+select date(created_at), platform, count(distinct user_id) as dau
 from gameplays
+group by 1, 2
+order by 1, 2;
+
+-- Daily ARPPU - Daily Average Revenue Per Purchasing User
+select date(created_at), round(sum(price) / count(distinct user_id), 2) as arppu
+from purchases
+where refunded_at is	 null
 group by 1
 order by 1;
