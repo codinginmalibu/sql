e6c021b1c95b651493d19d3dc41169b02e8c0e68
Shailen Mistry <shailen@gmail.com>
2017-05-29 11:51:23 -0400

    string and math functions

---
+++ bakeries.sql
@@ -0,0 +1,35 @@
+SELECT * FROM bakeries;
+
+SELECT * FROM bakeries LIMIT 10;
+
+
+-- Datetime functions
+
+SELECT DATETIME(delivery_time)
+FROM baked_goods;
+
+SELECT DATE(delivery_time), count(*) as count_baked_goods
+FROM baked_goods
+GROUP BY DATE(delivery_time);
+
+SELECT DATETIME(manufacture_time, '+2 hours', '30 minutes', '1 day') as inspection_time
+FROM baked_goods;
+
+SELECT DATETIME(delivery_time, '+5 hours', '20 minutes', '2 day') as package_time
+FROM baked_goods;
+
+
+-- Math functions
+
+SELECT ROUND(ingredients_cost, 2) as rounded_cost
+FROM baked_goods;
+
+SELECT ROUND(distance,2) AS distance_from_market 
+FROM bakeries;
+
+SELECT id, MIN(cook_time, cool_down_time) AS max_time
+FROM baked_goods ORDER BY max_time DESC;
+
+
+
+
